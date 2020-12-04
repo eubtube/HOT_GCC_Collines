17636dab16b9dc31196d2686aac420e5b064a3d6
eubtube <eubank.aaron@gmail.com>
2020-12-03 21:33:00 -0500

    more updates to add to proj files

--- scripts/add_to_qgisproject.py
+++ scripts/add_to_qgisproject.py
@@ -64,83 +64,93 @@ from qgis.gui import (
 ###### Add Vector Layers
 
 # Add and style buildings
-def add_vector_layers(qpid, pathv):
+def add_vector_layers(project, qpid, pathv):
+    '''adds building, border layer to qgis project, returns building layer'''
     #print(pathv)
-    buildingfile = os.path.join(pathv, '{}_buildings.gpkg'.format(qpid))
+    buildingfile = os.path.join(pathv, '{}_buildings.gpkg'.format(qpid)) # not very functional, assumes knowledge that user might not. Make an optional parameter
     print(buildingfile)
-    layer1 = QgsVectorLayer(buildingfile, "Buildings", "ogr")
-    if not layer1 or not layer1.isValid():
-        print("Layer failed to load!")
+    building_layer = QgsVectorLayer(buildingfile, "Buildings", "ogr") # accepts a geopackage, returns a layer
+    if not building_layer or not building_layer.isValid():
+        print("Building layer failed to load!")
 
     # adds layer to the canvas
-    QgsProject.instance().addMapLayer(layer1, True)
+    #QgsProject.instance().addMapLayer(building_layer, True)
+    project.addMapLayer(building_layer, True)
 
     # Verifies that layer was read
-    print(layer1.displayField())
+    print(building_layer.displayField())
 
-    symbol = QgsFillSymbol.createSimple(mylayers.red_style)
-    # Pulled from previous version (but still doesn't ):
-    #renderer = layer1.renderer()
-    #print("Type:", renderer.type())
-
-    #renderer1 = layer1.renderer().setSymbol(symbol)
+    print(mylayers.red_style)
+    building_symbol = QgsFillSymbol.createSimple(mylayers.red_style)
+    print(building_symbol)
+    #renderer1 = building_layer.renderer().setSymbol(symbol)
     #pdb.set_trace()
     #print(dir(renderer1))
-    layer1.renderer().setSymbol(symbol)
-    layer1.triggerRepaint()
+    building_layer.renderer().setSymbol(building_symbol)
+    building_layer.triggerRepaint()
 
     # Add and style border layer
     borderfile = os.path.join(pathv, 'border.gpkg|layername=border')
-    layer2 = QgsVectorLayer(borderfile, "Border", "ogr")
-    if not layer2 or not layer2.isValid():
-        print("Layer failed to load!")
+    border_layer = QgsVectorLayer(borderfile, "Border", "ogr")
+    if not border_layer or not border_layer.isValid():
+        print("Border layer failed to load!")
 
-    QgsProject.instance().addMapLayer(layer2, True)
+    # adds layer to the canvas
+    #QgsProject.instance().addMapLayer(border_layer, True)
+    project.addMapLayer(border_layer, True)
 
     # Verifies that layer was read
-    print(layer2.displayField())
+    print(building_layer.displayField())
 
-    symbol = QgsFillSymbol.createSimple(mylayers.yellow_style)
-    layer2.renderer().setSymbol(symbol)
-    layer2.triggerRepaint()
+    print(mylayers.yellow_style)
+    border_symbol = QgsFillSymbol.createSimple(mylayers.yellow_style)
+    print(border_symbol)
+    border_layer.renderer().setSymbol(border_symbol)
+    border_layer.triggerRepaint()
 
-    return layer1, layer2
+    # Returns building layer, border layer added to QGIS project only
+    return building_layer
 
-# Sets the extent to the clipped buildings
-
-def set_extent():
-    layer1, layer2 = add_vector_layers(qpid, pathv)
-    layer1.selectAll()
+def set_extent(layer):
+    '''Sets extent of QGIS canvas to a layer'''
+    layer.selectAll()
     canvas = QgsMapCanvas()
-    canvas.zoomToSelected(layer1)
-    layer1.removeSelection()
+    canvas.zoomToSelected(layer)
+    layer.removeSelection()
 
 ######## Add Raster Layers
-
-def add_tile_group(pathr):
+# TODO: Make general enough to add any raster layer, only pass in orthos
+# select .tifs and filter out dem in "build_proj"
+def add_tile_group(project, root, pathr, group_name):
+    '''adds project Maxar tiles to QGIS project. var root is a layer tree '''
     # Set up Tiles Group
-    root = QgsProject.instance().layerTreeRoot()
-    tiles_group = root.insertGroup(-1, "Tiles")
 
-    # Adds just the tiles to the group
+    tiles_group = root.insertGroup(-1, group_name)
+
+    # Adds just the Maxar tiles to the group
     for filename in os.listdir(pathr):
-        if (filename.endswith('.tif')) & (('dem' in filename) == False): # only files that do not have "dem in their name"
+        if (filename.endswith('.tif')) & (('dem' in filename) == False): # grabs only files that do not have "dem" in their name
             tile = os.path.join(pathr, filename)
-            rlayer = QgsRasterLayer(tile, os.path.splitext(filename)[0])  # Os.path - get only extension
-            QgsProject.instance().addMapLayer(rlayer, False)
-            root.insertChildNode(-1, QgsLayerTreeLayer(rlayer))
-            node_rlayer = QgsLayerTreeLayer(rlayer)
-            tiles_group.insertChildNode(0, node_rlayer)
-            root.removeLayer(rlayer)
+            # reads the raster layer object
+            ortho_layer = QgsRasterLayer(tile, os.path.splitext(filename)[0])  # Os.path - get only extension
+            #QgsProject.instance().addMapLayer(ortho_layer, False)
+            project.addMapLayer(ortho_layer, False)
+            root.insertChildNode(-1, QgsLayerTreeLayer(ortho_layer))
+            node_ortho_layer = QgsLayerTreeLayer(ortho_layer)
+            tiles_group.insertChildNode(0, node_ortho_layer)
+            root.removeLayer(ortho_layer)
         else:
             pass
 
 
 # Adds the DEM below
-def add_dem(qpid, pathr):
+# TODO: combine with function above, make add raster layers
+def add_dem(project, root, qpid, pathr):
+    ''' '''
     dem = os.path.join(pathr, '{}_dem.tif'.format(qpid))
     dem_layer = QgsRasterLayer(dem, '{}_dem'.format(qpid))
-    QgsProject.instance().addMapLayer(dem_layer, False)
+    #QgsProject.instance().addMapLayer(dem_layer, False)
+    project.addMapLayer(dem_layer, False)
     root.insertChildNode(-1, QgsLayerTreeLayer(dem_layer))
 
 
@@ -148,8 +158,7 @@ def add_dem(qpid, pathr):
 # Set up 3D view
 Qgs3D.initialize()
 '''
-def build_proj(qpid, pdir, pathv, pathr):
-
+def build_proj(qpid, pdir, pathv, pathr, template):
 
     QgsApplication.setPrefixPath("/usr/bin/qgis", True)
     qgs = QgsApplication([], False) # "False" prevents the gui from opening
@@ -157,21 +166,25 @@ def build_proj(qpid, pdir, pathv, pathr):
 
     # Will need to start a project
     project = QgsProject.instance()
-    project.read('~/files/3d_template_clean.qgs') # How to generalize this??  May be OK
+    project.read(template) # How to generalize this??  May be OK
+
+    # Creates a layer tree in the QGIS project called root
+    #root = QgsProject.instance().layerTreeRoot()
+    root = project.layerTreeRoot()
 
     # Adds vector layers
-    add_vector_layers(qpid, pathv)
+    building_layer = add_vector_layers(project, qpid, pathv)
 
     # Sets extent to building layer
-    set_extent()
+    set_extent(building_layer)
 
     # Adds all tiles as a grouped file in TOC
-    add_tile_group(pathr)
+    add_tile_group(project, root, pathr, "Tiles")
 
     # Adds DEM below Tiles
-    add_dem(qpid)
+    add_dem(project, root, qpid, pathr)
 
-    # Prints the
+    # Prints what's in the project
     print(QgsProject.instance().mapLayers().values())
 
     # Write to the QGIS Project, commented out when testing to avoid overwriting blank file
@@ -187,9 +200,10 @@ if __name__ == '__main__':
     parser.add_argument("-rd", "--raster_directory", help = "Directory path containing your raster files.")
     parser.add_argument("-prid", "--project_id", help = "Your project ID.")
     parser.add_argument("-pd", "--project_directory", help = "Directory path containing all you project files.")
+    parser.add_argument("-qt", "--qgis_template", help = "QGIS project template")
 
     options = parser.parse_args()
     #print(options)
     #print(type(options))
 
-    build_proj(options.project_id, options.project_directory, options.vector_directory, options.raster_directory)
+    build_proj(options.project_id, options.project_directory, options.vector_directory, options.raster_directory, options.qgis_template)
